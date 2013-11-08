f14a57c70c1dd620c4d8a2f9f9e93c2620fa4dec
Justin Michalicek <jmichalicek@gmail.com>
2013-11-08 18:16:29 +0000

    Added caching config for production

--- bash_shell_net/bash_shell_net/settings/production.py
+++ bash_shell_net/bash_shell_net/settings/production.py
@@ -1,3 +1,24 @@
 from .base import *
 
 TEMPLATE_DEBUG = DEBUG = False
+ALLOWED_HOSTS = ['bash-shell.net']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+)
