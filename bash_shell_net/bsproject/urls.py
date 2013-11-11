fdcee779276cbb34d47242acbd37963185587fbf
Justin Michalicek <jmichalicek@gmail.com>
2013-11-11 03:58:10 +0000

    Removed django.conf.urls.defaults imports

--- bash_shell_net/bsproject/urls.py
+++ bash_shell_net/bsproject/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls  import *
 
 urlpatterns = patterns('',
                        url(r'^(?P<project_name>.+)/$', 'bsproject.views.project', name='bsproject_project'),
