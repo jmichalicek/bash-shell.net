7a99846ec79908b81626e0ef5b475258b0c12c2f
Justin Michalicek <jmichailcek@gmail.com>
2017-01-21 23:58:20 -0500

    actual working docker setup for windows hosts

--- docker-dev.bat
+++ docker-dev.bat
@@ -1,8 +1,11 @@
 docker rm bsdev
 docker build --rm -f docker/Dockerfile.dev -t bash-shell-net:dev .
-docker run -ti --rm -v %CD%:/home/developer/bash-shell.net/ ^
+
+set code_dir=%CD%
+
+docker run -ti --rm ^
+    -v %code_dir%:/home/developer/bash-shell.net/ ^
     -v bsdev_pyenv:/home/developer/.pyenv ^
     --name=bsdev ^
     -p 8000:8000 ^
-    bash-shell-net:dev ^
-    bash -c 'bash $HOME/docker_entrypoints/dev_entrypoint.sh'
+    bash-shell-net:dev bash -c "bash $HOME/docker_entrypoints/dev_entrypoint.sh"
--- docker/dev_entrypoint.sh
+++ docker/dev_entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 sudo service postgresql start
-
-psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='developer'" | grep -q 1 || sudo -u postgres createuser -s developer
-psql postgres -tAc "SELECT 1 from pg_database WHERE datname='bsdev'" | grep -q 1 || sudo -u postgres createdb bsdev -O developer
+psql -U postgres -h 127.0.0.1 -tAc "SELECT 1 FROM pg_roles WHERE rolname='developer'" | grep -q 1 || sudo -u postgres createuser -s developer
+psql -U postgres -h 127.0.0.1 -tAc "SELECT 1 from pg_database WHERE datname='bsdev'" | grep -q 1 || sudo -u postgres createdb bsdev -O developer
 echo 'bsdev' > ~/bash-shell.net/.python-version
 cd ~/bash-shell.net/ && pip install -r requirements.txt && python manage.py migrate
 exec $SHELL
