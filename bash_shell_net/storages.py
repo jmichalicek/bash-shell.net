4bb78132bd9b7739ac7f5524059b9b96ec2691b3
Justin Michalicek <jmichalicek@gmail.com>
2017-08-28 00:13:19 +0000

    tweak staticfiles storage to hopefully play more nicely with issues caused by ckeditor until I get rid of it

--- bash_shell_net/storages.py
+++ bash_shell_net/storages.py
@@ -6,6 +6,8 @@ from django.contrib.staticfiles.storage import CachedFilesMixin
 class StaticStorage(CachedFilesMixin, S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     file_overwrite_file = True
+    max_post_process_passes = 15  # ckeditor is not playing nicely with the default 5, set it high
+    manifest_strict = False  #hopefully stop things from blowing up if the file is missing.  eventually remove these two
 
 
 class MediaStorage(S3Boto3Storage):
