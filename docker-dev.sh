99f81801507bc6d319f9919545a577bb8be8869a
Justin Michalicek <jmichalicek@gmail.com>
2017-09-27 22:34:58 -0400

    dev docker setup working with pipenv

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 docker rm bsdev
-docker build --rm -f docker/Dockerfile -t bash-shell-net:dev .
+docker build --rm -f docker/Dockerfile.dev -t bash-shell-net:dev .
 docker run -ti --rm -v `pwd`:/home/developer/bash-shell.net/ \
-    -v bsdev_pyenv:/home/developer/.pyenv \
+    -v bsdev_virtulaenvs:/home/developer/.local/share/virtualenvs \
     --name=bsdev \
     -p 8000:8000 \
-    bash-shell-net:dev bash -c 'bash $HOME/docker_entrypoints/dev_entrypoint.sh'
+    bash-shell-net:dev bash -c 'bash /home/developer/docker_entrypoints/dev_entrypoint.sh'
