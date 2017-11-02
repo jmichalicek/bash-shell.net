52ac30ed5921e53430fe615e22b90eb1179fdfb1
Justin Michalicek <jmichalicek@gmail.com>
2017-11-01 21:28:46 -0400

    tweak docker-dev.bat to have fully set paths

--- docker-dev.bat
+++ docker-dev.bat
@@ -5,7 +5,7 @@ set code_dir=%CD%
 
 docker run -ti --rm ^
     -v %code_dir%:/home/developer/bash-shell.net/ ^
-    -v bsdev_pyenv:/home/developer/.pyenv ^
+    -v bsdev_virtulaenvs:/home/developer/.local/share/virtualenvs ^
     --name=bsdev ^
     -p 8000:8000 ^
-    bash-shell-net:dev bash -c "bash $HOME/docker_entrypoints/dev_entrypoint.sh"
+    bash-shell-net:dev bash -c "bash /home/developer/docker_entrypoints/dev_entrypoint.sh"
