799e05e41560150300c29cb59f171fc5700ebf70
Justin Michalicek <jmichalicek@gmail.com>
2017-11-05 10:26:40 -0500

    about and opensource pages as TemplateView with static templates

--- bash_shell_net/urls.py
+++ bash_shell_net/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 from django.contrib.flatpages.sitemaps import FlatPageSitemap
 from django.contrib.sitemaps import GenericSitemap
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 from .sitemaps import *
 from .feeds import BlogFeedRss
@@ -21,8 +22,10 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
-    url(r'^feeds/rss/',BlogFeedRss()),
+    url(r'^feeds/rss/', BlogFeedRss()),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^opensource/$', TemplateView.as_view(template_name="open_source.html"), name="opensource"),
     url(r'^blog/', include('blog.urls')),
     url(r'^projects/', include('projects.urls')),
-    url(r'^',include('blog.urls')),
+    url(r'^', include('blog.urls')),
 ]
