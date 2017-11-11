e44a8ffad7c1aeba396ab5de132367a27c6ef3dd
Justin Michalicek <jmichalicek@gmail.com>
2017-11-11 10:15:45 -0500

    stop tagging release version in sentry for now

--- bash_shell_net/settings/production.py
+++ bash_shell_net/settings/production.py
@@ -48,5 +48,5 @@ RAVEN_CONFIG = {
     'dsn': os.environ.get('SENTRY_DSN', ''),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
 }
