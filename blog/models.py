6850c1fa162cadde18750ee7b1a350b17daad060
Justin Michalicek <jmichalicek@gmail.com>
2017-11-14 22:23:02 -0500

    add a logged in use preview view for unpublished posts

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import, unicode_literals, division
-
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils import timezone
 from django.utils.text import slugify
 
 from .managers import PublishedPostQuerySet
@@ -32,18 +31,25 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('blog.Tag', blank=True, related_name='posts')
-    is_published = models.BooleanField(db_index=True, default=False, blank=True)
-    published_date = models.DateTimeField(db_index=True, null=True, default=None, blank=True)
-    user = models.ForeignKey('accounts.User', null=True, on_delete=models.SET_NULL, default=None,
-                             blank=True, db_index=True, related_name='posts')
+    is_published = models.BooleanField(
+        db_index=True, default=False, blank=True)
+    published_date = models.DateTimeField(
+        db_index=True, null=True, default=None, blank=True)
+    user = models.ForeignKey(
+        'accounts.User',
+        null=True,
+        on_delete=models.SET_NULL,
+        default=None,
+        blank=True,
+        db_index=True,
+        related_name='posts')
 
     slug = models.SlugField(
         help_text='Automatically built from the title.',
         db_index=True,
         blank=True,
         default='',
-        max_length=100
-    )
+        max_length=100)
 
     objects = PublishedPostQuerySet.as_manager()
 
@@ -53,7 +59,11 @@ class Post(models.Model):
         # become unreachable or everything has to be done in UTC and then the web browser
         # set date and time is incorrect when creating a post.  So allow proper user timezone
         # but create the url always as UTC
-        return reverse('blog_post_detail', args=[self.slug])
+        if self.is_published and self.published_date and self.published_date <= timezone.now(
+        ):
+            return reverse('blog_post_detail', args=[self.slug])
+        else:
+            return reverse('blog_post_preview', args=[self.slug])
 
     def __str__(self):
         return self.title
