747372af684323f1410b35838704e90f8c30882e
Justin Michalicek <jmichalicek@gmail.com>
2017-11-16 22:02:04 -0500

    pep8 lines and unnecessary __future__ imports

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import UserManager
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
