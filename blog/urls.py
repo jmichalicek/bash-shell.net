1d4df2771cfb2646e80669b9069fab4280b6711c
Justin Michalicek <jmichalicek@gmail.com>
2018-02-11 11:06:11 -0500

    Actually use the PostPreviewView for previewing

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^archive/$', views.PostArchiveView.as_view(), name='blog_post_archive'),
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
         views.PostDetailView.as_view(), name='legacy_post_detail'),
-    url(r'^preview/(?P<slug>[\w_-]+)/$', views.PostDetailView.as_view(), name='blog_post_preview'),
+    url(r'^preview/(?P<slug>[\w_-]+)/$', views.PostPreviewView.as_view(), name='blog_post_preview'),
     url(r'^(?P<slug>[\w_-]+)/$', views.PostDetailView.as_view(), name='blog_post_detail'),
     url(r'^$', views.PostListView.as_view(), name='blog_post_list'),
     ]
