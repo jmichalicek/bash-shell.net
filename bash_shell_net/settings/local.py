aef31080cd766d7bd86cc4f41dec09c749ab95b6
Justin Michalicek <jmichalicek@gmail.com>
2018-11-21 02:15:58 +0000

    initial wagtail config

--- bash_shell_net/settings/local.py
+++ bash_shell_net/settings/local.py
@@ -1,3 +1,4 @@
 from .base import *
 
 INSTALLED_APPS += ('django_coverage', )
+WAGTAIL_SITE_NAME = 'bash-shell.net development'
--- bash_shell_net/urls.py
+++ bash_shell_net/urls.py
@@ -7,23 +7,34 @@ from django.contrib.sitemaps import GenericSitemap
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic import TemplateView
 
+from django.urls import path, re_path, include
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
+
 from .feeds import BlogFeedRss
 from .sitemaps import BlogSitemap, ProjectSiteMap, ProjectsSiteMap
 
 admin.autodiscover()
 
-sitemaps = {'blog': BlogSitemap,
-            'project': ProjectSiteMap,
-            'projects': ProjectsSiteMap}
+sitemaps = {'blog': BlogSitemap, 'project': ProjectSiteMap, 'projects': ProjectsSiteMap}
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
+    url(r'^sitemap\.xml$', sitemap, {
+        'sitemaps': sitemaps
+    }),
     url(r'^feeds/rss/', BlogFeedRss()),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
     url(r'^opensource/$', TemplateView.as_view(template_name="open_source.html"), name="opensource"),
     url(r'^blog/', include('blog.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
