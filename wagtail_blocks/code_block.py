cd29d280de6d5ba0a2a0821a49beb5625e7e7228
Justin Michalicek <jmichalicek@gmail.com>
2018-12-02 04:02:05 +0000

    Began adding new wagtail block for highlighted code

---
+++ wagtail_blocks/code_block.py
@@ -0,0 +1,63 @@
+from django.utils.safestring import mark_safe
+
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name
+
+from wagtail.wagtailcore import blocks
+
+
+class CodeBlock(blocks.StructBlock):
+    """
+    Code Highlighting Block.
+    """
+
+    LANGUAGE_CHOICES = (
+        ('bash', 'Bash/Shell'),
+        ('batch', 'Batch'),
+        ('c', 'C'),
+        ('cpp', 'C++'),
+        ('csharp', 'C#'),
+        ('css', 'CSS'),
+        ('docker', 'Dockerfile'),
+        ('elixir', 'Elixir'),
+        ('go', 'Go'),
+        ('graphql', 'GraphQL'),
+        ('htmldjango', 'Django Template'),
+        ('html', 'HTML'),
+        ('javascript', 'JavaScript'),
+        ('json', 'JSON'),
+        ('lisp', 'Lisp'),
+        ('make', 'Makefile'),
+        ('perl', 'Perl'),
+        ('php', 'PHP'),
+        ('python', 'Python'),
+        ('jsx', 'React JSX'),
+        ('ruby', 'Ruby'),
+        ('sql', 'SQL'),
+        ('yaml', 'YAML'),
+    )
+
+    language = blocks.ChoiceBlock(choices=LANGUAGE_CHOICES)
+    code = blocks.TextBlock()
+
+    class Meta:
+        """
+        Set streamfield icon.
+        """
+        icon = 'code'
+
+    def render(self, value, context=None):
+        """Render codeblock."""
+        src = value['code'].strip('\n')
+        lang = value['language']
+
+        lexer = get_lexer_by_name(lang)
+        formatter = get_formatter_by_name(
+            'html',
+            linenos=None,
+            cssclass='syntax',
+            style='default',
+            noclasses=False,
+        )
+        return mark_safe(highlight(src, lexer, formatter))
