3b69f291f55b535b6f33e0f893a6d70f69de5e1e
Justin Michalicek <jmichalicek@gmail.com>
2020-02-16 09:11:18 -0500

    fix staticfiles storage settings

--- bash_shell_net/storages.py
+++ bash_shell_net/storages.py
@@ -4,9 +4,12 @@ from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-    file_overwrite = True
+# Really should remove this, doubt I'll ever use it again.
+# Leaving it here caused it to initialize in production even when not referenced
+# which caused exceptions to be raised wit the STATICFILES_LOCATION setting removed
+# class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
+#     location = settings.STATICFILES_LOCATION
+#     file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
