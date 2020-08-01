75f2f311b6b6acf4709f36169e4f218570785b85
Justin Michalicek <jmichalicek@gmail.com>
2020-08-01 17:41:03 +0000

    improve the sitemap and include on-tap stuff

--- bash_shell_net/sitemaps.py
+++ bash_shell_net/sitemaps.py
@@ -2,17 +2,76 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 from blog.models import BlogPage
+from on_tap.models import OnTapPage, RecipePage, BatchLogPage
 from projects.models import Project
 from wagtail.contrib.sitemaps import Sitemap as WagtailSitemap
+from wagtail.core.models import Orderable, Page
 
 
 class BlogSitemap(WagtailSitemap):
-    """Sitemap for posts"""
-    changefreq = "never"
+    """
+    Sitemap for posts
+    """
+
     priority = 0.5
+    limit = 50
 
     def items(self):
-        return BlogPage.objects.live().order_by('first_published_at')
+        return BlogPage.objects.live().public().order_by('-first_published_at')
+
+    def lastmod(self, obj):
+        return obj.last_published_at
+
+
+class OnTapSitemap(WagtailSitemap):
+    """
+    Sitemap for posts
+    main_298a6006830dacabb4569bb44b23e66b2a7b1150
+    """
+
+    # changefreq = "never"
+    limit = 50
+    priority = 0.5
+    protocol = 'https'
+
+    def items(self):
+        return OnTapPage.objects.live().public().order_by('-first_published_at')
+
+    def lastmod(self, obj):
+        return obj.last_published_at
+
+
+class RecipePageSitemap(WagtailSitemap):
+    """
+    Sitemap for posts
+    main_298a6006830dacabb4569bb44b23e66b2a7b1150
+    """
+
+    # changefreq = "never"
+    limit = 50
+    priority = 0.5
+    protocol = 'https'
+
+    def items(self):
+        return RecipePage.objects.live().public().order_by('-first_published_at')
+
+    def lastmod(self, obj):
+        return obj.last_published_at
+
+
+class BatchLogPageSitemap(WagtailSitemap):
+    """
+    Sitemap for posts
+    main_298a6006830dacabb4569bb44b23e66b2a7b1150
+    """
+
+    # changefreq = "never"
+    limit = 50
+    priority = 0.5
+    protocol = 'https'
+
+    def items(self):
+        return BatchLogPage.objects.live().public().order_by('-first_published_at')
 
     def lastmod(self, obj):
         return obj.last_published_at
@@ -20,8 +79,9 @@ class BlogSitemap(WagtailSitemap):
 
 class ProjectSiteMap(Sitemap):
     """List the /blog/projects/ page"""
-    changefreq = "weekly"
+
     priority = 0.5
+    limit = 50
 
     def items(self):
         return [reverse('projects_project_list')]
@@ -32,11 +92,14 @@ class ProjectSiteMap(Sitemap):
 
 
 class ProjectsSiteMap(Sitemap):
-    """Lists posts in the projects cateogry.  This is
+    """
+    Lists posts in the projects cateogry.  This is
     to separate them out from the blog posts since the url
-    may change for them."""
-    changefreq = "monthly"
+    may change for them.
+    """
+
     priorit = 0.5
+    limit = 50
 
     def items(self):
         return Project.objects.all()
--- bash_shell_net/urls.py
+++ bash_shell_net/urls.py
@@ -1,38 +1,42 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.contrib.sitemaps.views import sitemap
+from wagtail.contrib.sitemaps.views import sitemap, index
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from .feeds import BlogFeedRss
-from .sitemaps import BlogSitemap, ProjectSiteMap, ProjectsSiteMap
+from .sitemaps import BlogSitemap, ProjectSiteMap, ProjectsSiteMap, OnTapSitemap, RecipePageSitemap, BatchLogPageSitemap
 
 admin.autodiscover()
 
-sitemaps = {'blog': BlogSitemap, 'project': ProjectSiteMap, 'projects': ProjectsSiteMap}
+sitemaps = {
+    'on_tap': OnTapSitemap,
+    'on_tap-batches': BatchLogPageSitemap,
+    'on_tap-recipes': RecipePageSitemap,
+    'blog': BlogSitemap,
+    'project': ProjectSiteMap,
+    'projects': ProjectsSiteMap,
+}
 
 urlpatterns = [
     re_path(r'^cms/', include(wagtailadmin_urls)),
     re_path(r'^documents/', include(wagtaildocs_urls)),
-    # re_path(r'^pages/', include(wagtail_urls)),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    url(r'^sitemap\.xml$', sitemap, {
-        'sitemaps': sitemaps,
-    }),
-    url(r'^feeds/rss/', BlogFeedRss()),
-    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
-    url(r'^opensource/$', TemplateView.as_view(template_name="open_source.html"), name="opensource"),
-    # url(r'^oldblog/', include('blog.urls')),
-    url(r'^archives/', include('blog.urls')),
-    url(r'^projects/', include('projects.urls')),
-    url(r'^', include(wagtail_urls)),
-    # url(r'^', include('blog.urls')), # legacy stuff
+    path('sitemap.xml', index, {'sitemaps': sitemaps,}),
+    path('sitemap-<section>.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    re_path(r'^feeds/rss/', BlogFeedRss()),
+    re_path(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    re_path(r'^opensource/$', TemplateView.as_view(template_name="open_source.html"), name="opensource"),
+    re_path(r'^archives/', include('blog.urls')),
+    re_path(r'^projects/', include('projects.urls')),
+    re_path(r'^', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
