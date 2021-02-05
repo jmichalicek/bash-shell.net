810c32728bed773b083383823b74c5e041f9d50b
Justin Michalicek <jmichalicek@gmail.com>
2021-02-05 16:30:41 +0000

    upgrade to wagtail 2.12 and django 3.1.6

--- bash_shell_net/settings/base.py
+++ bash_shell_net/settings/base.py
@@ -286,7 +286,14 @@ if TESTING:
     PASSWORD_HASHERS = [
         'django.contrib.auth.hashers.MD5PasswordHasher',
     ]
-    # Should have made it so that the whitenoise stuff is not used in tests and I do not need
+
+    # For django_coverage_plugin
+    TEMPLATES[0]['OPTIONS']['debug'] = True
+
+    WHITENOISE_AUTOREFRESH = True
+    WHITENOISE_USE_FINDERS = True
+    WHITENOISE_MANIFEST_STRICT = False
+    # The above should have made it so that the whitenoise stuff is not used in tests and I do not need
     # to run collectstatic to run tests, but I seem to be missing something still.
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-    WHITENOISE_AUTOREFRESH = False
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
