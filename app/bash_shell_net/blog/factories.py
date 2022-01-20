1f64af09ed9e3e846c79b4f23e331d1675e1b943
Justin Michalicek <jmichalicek@gmail.com>
2022-01-19 19:51:28 -0500

    Factories for blog app

--- app/bash_shell_net/blog/factories.py
+++ app/bash_shell_net/blog/factories.py
@@ -1,1 +1,21 @@
 # TODO: wagtail factory_boy factory stuff.
+
+import factory
+import wagtail_factories
+
+from bash_shell_net.blog.models import BlogPage, BlogPageIndex
+
+
+class BlogPageIndexFactory(wagtail_factories.PageFactory):
+    title = 'Blog'
+
+    class Meta:
+        model = BlogPageIndex
+
+
+class BlogPageFactory(wagtail_factories.PageFactory):
+
+    title = factory.Sequence(lambda n: f'Blog Post {n}')
+
+    class Meta:
+        model = BlogPage
